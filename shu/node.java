163429543d5707107a3815a318c713b0b7551323
ncepulousongnan <52988635+ncepulousongnan@users.noreply.github.com>
2020-02-17 17:04:05 +0800

    Add files via upload

--- shu/node.java
+++ shu/node.java
@@ -1,105 +1,117 @@
 package shu;
+//数据结构实验——二叉树的遍历
+//节点类的创建，有三种遍历方法和二叉树的创建
 import java.util.Scanner;
-
 public class node {
- int data;
- node lnode;
- node rnode;
- Scanner h=new Scanner(System.in);
- public void setdata(int data)
- {
-	 this.data=data;
- }
- public int getdata()
- {
-	 return data;
- }
- public void setlnode(node lnode)
- {
-	 this.lnode=lnode;
- }
+	 int data;
+	 node lnode;
+	 node rnode;
+	 Scanner h=new Scanner(System.in);
+	 public void setdata(int data)
+	 {
+		 this.data=data;
+	 }
+	 public int getdata()
+	 {
+		 return data;
+	 }
+	 public void setlnode(node lnode)
+	 {
+		 this.lnode=lnode;
+	 }
 
- public node getlnode()
- {
-	 return lnode;
- }
- public void setrnode(node rnode)
- {
-	 this.rnode=rnode;
- }
- public node getrnode(node rnode)
- {
-	 return rnode;
- }
- public void front()
- {
-	 System.out.print(data+" ");
-	 if(lnode!=null)
+	 public node getlnode()
 	 {
-		 lnode.front();
+		 return lnode;
 	 }
-	 if(rnode!=null)
+	 public void setrnode(node rnode)
 	 {
-		 rnode.front();
+		 this.rnode=rnode;
 	 }
- }
- public void mid()
- {
-	 if(lnode!=null)
+	 public node getrnode()
 	 {
-		 lnode.mid();
+		 return rnode;
 	 }
-	 System.out.print(data+" ");
-	 if(rnode!=null)
+	 public void front(node n)//前序遍历
 	 {
-		 rnode.mid();
+		 System.out.print(n.getdata()+" ");
+		 if(n.getlnode()!=null)
+		 {
+			n.getlnode().front(n.getlnode());
+		 }
+		 if(n.getrnode()!=null)
+		 {
+			 n.getrnode().front(n.getrnode());
+		 }
 	 }
- }
- public void behide()
- {
-	 if(lnode!=null)
+	 public void mid(node n)//中序遍历
 	 {
-		 lnode.behide();
+		 if(n.getlnode()!=null)
+		 {
+			 n.getlnode().mid(n.getlnode());
+		 }
+		 System.out.print(n.getdata()+" ");
+		 if(n.getrnode()!=null)
+		 {
+			 n.getrnode().mid(n.getrnode());
+		 }
 	 }
-	 if(rnode!=null)
+	 public void behide(node n)//后序遍历
 	 {
-		 rnode.behide();
+		 if(n.getlnode()!=null)
+		 {
+			 n.getlnode().behide(n.getlnode());
+		 }
+		 if(n.getrnode()!=null)
+		 {
+			 n.getrnode().behide(n.getrnode());
+		 }
+		 System.out.print(n.getdata()+" ");
 	 }
-	 System.out.print(data+" ");
- }
-public void zuo(node node)
-{    int data=h.nextInt();
-
-    if(data==0)
-{ node.setlnode(null);
-int n=h.nextInt();
-    if(n==0)
-{ node.setrnode(null);
-}
-    else{node rnode=new node();
-    node.setrnode(rnode);
-    rnode.setdata(n);
-    rnode.zuo(rnode);
-    }
-}
-    else{
-    	node lnode=new node();
-	node.setlnode(lnode);  
-    lnode.setdata(data);
-    lnode.zuo(lnode);
-    int n=h.nextInt();
-    if(n==0)
-    { node.setrnode(null);
-    return;
-    }
-        else{node rnode=new node();
-        node.setrnode(rnode);
-        rnode.setdata(n);
-        rnode.zuo(rnode);
-        }
-    }
+	 public void zuo(node node)//创建二叉树
+	 {   
+		 int x=h.nextInt();
+		 if(x==0)
+		 {
+			 node.setlnode(null);
+			 int y=h.nextInt();
+			 if(y==0)
+			 {
+			 node.setrnode(null);
+			 }
+			 else
+			 {
+			 node q=new node();
+			 q.setdata(y);
+			 node.setrnode(q);
+			 q.zuo(q);
+			 } 
+		 }
+		 else
+		 {
+			 node p=new node();
+			 p.setdata(x);
+			 node.setlnode(p);
+			p.zuo(p);
+			if(node.getlnode()!=null)
+			 {   
+			 int y=h.nextInt();
+			 if(y==0)
+			 {
+			 node.setrnode(null);
+			 }
+			 else
+			 {
+			 node q=new node();
+			 q.setdata(y);
+			 node.setrnode(q);
+			 q.zuo(q);
+			 }
+			}
+		 }
+		 }
+		 
+	 }
 	
 	
 
-}
-}
--- shu/tree.java
+++ shu/tree.java
@@ -1,27 +1,26 @@
 package shu;
-
+//二叉树类，有三种遍历方法和二叉树的建立方法，都是调用根节点的方法
 public class tree {
-   node root;
- int n;
-public void setroot(node root)
-{
-	this.root=root;
-}
-public void qian()
-{
-root.front();
-}
-public void zhong()
-{
-	root.mid();
-}
-public void hou()
-{
-	root.behide();
-}
-public void settree(node root)
-{
-	root.zuo(root);
-}
-
+	 node root;
+	 int n;
+	public node getroot()
+	{
+	    return root;
+	}
+	public void qian(node root)
+	{
+	root.front(root);
+	}
+	public void zhong(node root)
+	{
+		root.mid(root);
+	}
+	public void hou(node root)
+	{
+		root.behide(root);
+	}
+	public void settree(node root)
+	{
+		root.zuo(root);
+	}
 }
